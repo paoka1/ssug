089fc90a7e5afbe1d2dcf619405b8cbfe1cc44bf
paoka1 <paoka@foxmail.com>
2023-12-23 12:40:36 +0800

    Feat 返回短链过期时间

--- internal/utils/result.go
+++ internal/utils/result.go
@@ -1,12 +1,12 @@
 package utils
 
 type Result struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data string `json:"data"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
 
-func ResultSuccess(data string) Result {
+func ResultSuccess(data interface{}) Result {
 	return Result{
 		Code: 0,
 		Msg:  "操作成功",
@@ -22,7 +22,8 @@ func ResultFail(code int, msg string) Result {
 	}
 }
 
-func ResultFailD(code int, msg string, data string) Result {
+// ResultFailWD Result Fail With Data
+func ResultFailWD(code int, msg string, data interface{}) Result {
 	return Result{
 		Code: code,
 		Msg:  msg,
